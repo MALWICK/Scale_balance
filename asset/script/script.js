5d1a9c87e87fbfc19d964897350a020403dbbf5e
MALWICK <batahumphrey66@gamil.com>
2023-06-16 22:34:09 +0100

    made the clear button to work

--- asset/script/script.js
+++ asset/script/script.js
@@ -1,20 +1,20 @@
-const input1 = document.querySelector('.scale1');
-const input2 = document.querySelector('.scale2');
-const submit = document.querySelector('.btn1');
-const result = document.querySelector('.paragraph1');
-const resultdiv = document.querySelector('.results');
-const refreshbtn = document.querySelector('.refreshbtn');
+const input1 = document.querySelector(".scale1");
+const input2 = document.querySelector(".scale2");
+const submit = document.querySelector(".btn1");
+const result = document.querySelector(".paragraph1");
+const resultdiv = document.querySelector(".results");
+const refreshbtn = document.querySelector(".refreshbtn");
 let value1;
 let value2;
 let number1 = [];
 let listNum = [];
 
-submit.addEventListener('click', function () {
+submit.addEventListener("click", function () {
   balanceNum();
 });
 
 refreshbtn.addEventListener(
- ' click',
+  "click",
   () => {
     window.location.reload();
   },
@@ -25,8 +25,8 @@ function balanceNum() {
   value1 = input1.value; //here .value means we are getting the value of its number
   console.log(value1);
   value2 = input2.value;
-  number1 = value1.split(','); // the split with the comma put  the numbers as single index without the comma the numbers will be joined
-  listNum = value2.split(',');
+  number1 = value1.split(","); // the split with the comma put  the numbers as single index without the comma the numbers will be joined
+  listNum = value2.split(",");
   for (let i = 0; i < listNum.length; i++) {
     for (let j = 0; j < listNum.length; j++) {
       if (
